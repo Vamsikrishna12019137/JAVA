7e6c90da68227e27c6ca9010d259fa76b339aed3
Vamsikrishna12019137 <123366529+Vamsikrishna12019137@users.noreply.github.com>
2023-02-23 23:00:36 +0530

    Create cse310_week5_day4_ques1.java

---
+++ cse310_week5_day4_ques1.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static void main(String... vamsi) {
+        int n = 2;
+        int result = factorial(n);
+        System.out.println(result);
+    }
+}
