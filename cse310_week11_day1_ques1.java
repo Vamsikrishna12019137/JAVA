94f114c3f31bca153e6d328aee6d1198cf3e6d61
Venkata Vamsi Krishna <123366529+Vamsikrishna12019137@users.noreply.github.com>
2023-04-04 10:32:26 +0530

    Create cse310_week11_day1_ques1.java
    Create a class Person having a single attribute: age. Now, create a functional interface with the following method in it: void find(Person p[]); Create a Demo class having main() method and ask the user to createa and initialize "n" Person objects (where "n" is input given by user). Now, use the lambda expression that will use this find() method to find out how many Persons have age greater than 25.
    
    Input Format
    
    Your program will take 2 types of inputs. First user input will be no. of person objects "n" to be created. Remaining user inputs will be "age" value of every Person object created. If value of "n" entered is less than 1, then display message "Invalid input" and no further input should be taken. Otherwise, display no. of persons having age greater than 25.
    
    Constraints
    
    Value of "n" and "age" should be greater than 0. If any invalid age (e.g. -2) is entered the user, then take 18 as the default age.
    
    Output Format
    
    Display no. of persons having age greater than 25. If value of "n" (i.e. no. of person objects) entered is less than 1, then display message "Invalid input".
    
    Sample Input 0
    
    3
    26
    14
    30
    Sample Output 0
    
    2

---
+++ cse310_week11_day1_ques1.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String... vamsi) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        if (n>1)
+        {
+            int count = 0;
+            int arr[] = new int[n];
+            for (int i=0;i<n;i++)
+            {
+                arr[i] = scan.nextInt();
+            }
+             for (int i=0;i<n;i++)
+            {
+                 if (arr[i]>25)
+                     count++;
+            }
+            System.out.println(count);
+        }
+        else
+        {
+                        System.out.println("Invalid Input");
+
+        }
+    }
+}
